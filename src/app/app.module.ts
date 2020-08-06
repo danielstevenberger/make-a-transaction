b651aad36db283fd424c2f50ca4f758a7bdc23ca
danielstevenberger <daniel.berger95@gmail.com>
2020-08-06 16:58:55 -0400

    Added resuable cards and a card container.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
 import { NavbarComponent } from "./navbar/navbar.component";
-import { NavbarLogoComponent } from './navbar/navbar-logo/navbar-logo.component';
+import { NavbarLogoComponent } from "./navbar/navbar-logo/navbar-logo.component";
+import { CardContainerComponent } from "./card-container/card-container.component";
+import { CardComponent } from './card-container/card/card.component';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, NavbarLogoComponent],
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    NavbarLogoComponent,
+    CardContainerComponent,
+    CardComponent,
+  ],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
